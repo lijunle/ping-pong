238fcd677fe1787a0e236168da812ea9acb79abe
Junle <lijunle@gmail.com>
2015-10-11 21:58:51 +0800

    Add ping-pong logic to page.
    - It is coupled with azure service.
    - Using on-the-fly configuration can decouple with specified service
      provider (and abitiliy to test locally).

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 /* eslint-disable no-console */
 
 import express from 'express';
+import * as table from './azure/table';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  const tableName = 'ping';
+  table.ensure(tableName)
+  .then(() => table.query(tableName))
+  .then(data => res.send(data), error => res.send(error));
+});
+
+app.post('/ping', (req, res) => {
+  const tableName = 'ping';
+  const dateTime = new Date();
+  const record = {
+    PartitionKey: 'ping',
+    RowKey: dateTime.toString(),
+    type: 'ping',
+    time: dateTime,
+  };
+
+  table.ensure(tableName)
+  .then(() => table.insert(tableName, record))
+  .then(data => res.send(data), error => res.status(500).send(error.toString()));
 });
 
 app.listen(port, () => {
