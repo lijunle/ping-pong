2fc86ac1a8a1ccd0524fd7a9029908a4be6d26cf
Junle <lijunle@gmail.com>
2015-10-12 01:23:44 +0800

    Generate Azure entry file when deploy.
    - Add deploy-azure task to contain Azure deployment relative works.
    - Leverage deploy-azure task in Azure deployment script
    - Add `generateServer` task to generate `server.js` file.
    - Remove `server.js` file from git repository.

--- deploy/azure.sh
+++ deploy/azure.sh
@@ -113,7 +113,7 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install
-  eval $NPM_CMD run build
+  eval $NPM_CMD run deploy-azure
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
