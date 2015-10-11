90b9eacb551feea3f2d8f9b6cab0cdb7a8838ab8
Junle <lijunle@gmail.com>
2015-10-12 00:44:16 +0800

    Add generate job task to Azure tasks.
    - The `generateJob`task will generate a continuous job in App_Data folder.
    - Git ignore the App_Data folder.

--- deploy/azure-tasks.js
+++ deploy/azure-tasks.js
@@ -16,4 +16,19 @@ function generateServer() {
   return writeFile(serverPath, content);
 }
 
-gulp.task('azure-tasks', () => generateServer());
+function generateJob() {
+  const nodePath = path.resolve(__dirname, '../node.cmd');
+  const jobPath = path.resolve(__dirname, '../dist/job.js');
+  const runPath = path.resolve(__dirname, '../App_Data/jobs/continuous/depcheck/run.cmd');
+  const content = `"${nodePath}" "${jobPath}"`;
+  return writeFile(runPath, content);
+}
+
+function runTasks() {
+  return Promise.all([
+    generateServer(),
+    generateJob(),
+  ]);
+}
+
+gulp.task('azure-tasks', () => runTasks());
