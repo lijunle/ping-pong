8375c10291790694968d1b83710102f3dcf3abca
Junle <lijunle@gmail.com>
2015-10-12 23:01:57 +0800

    Add Azure table logger.
    - It logs info and upper message to Azure storage `log` table.
    - The log can seen using https://waztable.azurewebsites.net/

--- src/azure/logger.js
+++ src/azure/logger.js
@@ -1,8 +1,10 @@
 import winston from 'winston';
+import { AzureLogger } from 'winston-azuretable';
 
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({ level: 'debug', debugStdout: true }),
+    new AzureLogger({ partitionKey: 'depcheck' }),
   ],
 });
 
