004a798891330a1405f4188897527542ca7915dc
Junle <lijunle@gmail.com>
2015-10-13 22:10:30 +0800

    Enable Airbnb React linter.
    - Resolve the lint warnings.

--- src/views/index.js
+++ src/views/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { logger } from '../services';
 
 export default React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+  },
   render() {
     logger.debug('[view:index] render index view.');
 
